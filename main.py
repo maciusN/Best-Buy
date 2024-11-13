e8fdf50548feded23cabc2463329659492fe2fb0
Maciej <maciusngames@gmail.com>
2024-11-13 04:02:08 +0100

    Add main logic and finish program

--- main.py
+++ main.py
@@ -1,12 +1,75 @@
-import products 
+import products
 import store
 
-product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
-                products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                products.Product("Google Pixel 7", price=500, quantity=250),
-               ]
 
-store = store.Store(product_list)
-products = store.get_all_products()
-print(store.get_total_quantity())
-print(store.order([(products[0], 1), (products[1], 2)]))
+def main():
+
+    product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
+                    products.Product("Bose QuietComfort Earbuds",
+                                     price=250, quantity=500),
+                    products.Product("Google Pixel 7", price=500, quantity=250)
+                    ]
+
+    best_buy = store.Store(product_list)
+
+    def start(store_obj):
+        menu = """
+        Store Menu
+        ----------
+        1. List all products in store
+        2. Show total amount in store
+        3. Make an order
+        4. Quit"""
+        print(menu)
+
+    while True:
+        start(best_buy)
+
+        user_input = int(input("Please choose a number: "))
+
+        if user_input == 1:
+            print("------")
+            for product in product_list:
+                print(f"{product.name}, Price: ${
+                    product.price}, Quantity: {product.quantity}")
+            print("------")
+            print()
+        elif user_input == 2:
+            total_amount = 0
+            for product in product_list:
+                total_amount += product.quantity
+            print(f"Total of {total_amount} items in store")
+        elif user_input == 3:
+            total_payment = 0
+            print("------")
+            for product in product_list:
+                print(f"{product.name}, Price: ${
+                    product.price}, Quantity: {product.quantity}")
+            print("------")
+            print("When you want to finish order, enter empty text.")
+            while True:
+                print()
+                user_input_product = input("Which product do you want? ")
+                user_input_amount = input("What amount do you want? ")
+                if user_input_product == "1" or user_input_product == "MacBook Air M2":
+                    total_payment += product_list[0].price * \
+                        int(user_input_amount)
+                    print("Product added to list!")
+                elif user_input_product == "2" or user_input_product == "Bose QuietComfort Earbuds":
+                    total_payment += product_list[1].price * \
+                        int(user_input_amount)
+                    print("Product added to list!")
+                elif user_input_product == "3" or user_input_product == "Google Pixel 7":
+                    total_payment += product_list[2].price * \
+                        int(user_input_amount)
+                    print("Product added to list!")
+                elif user_input_product == "" and user_input_amount == "":
+                    break
+            print("********")
+            print(f"Order made! Total payment = ${total_payment}")
+        elif user_input == 4:
+            break
+
+
+if __name__ == "__main__":
+    main()
