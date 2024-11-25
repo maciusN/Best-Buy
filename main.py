62eea2ce2956ffdc3268101337ea961e3261c587
Maciej <maciusngames@gmail.com>
2024-11-25 13:56:18 +0100

    Fix bugs

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import products
 import store
 
 
+def start():
+    menu = """
+        Store Menu
+        ----------
+        1. List all products in store
+        2. Show total amount in store
+        3. Make an order
+        4. Quit"""
+    print(menu)
+
+
 def main():
     """
     Main function to initialize the store with products and display the store menu.
@@ -14,16 +25,6 @@ def main():
 
     best_buy = store.Store(product_list)
 
-    def start():
-        menu = """
-        Store Menu
-        ----------
-        1. List all products in store
-        2. Show total amount in store
-        3. Make an order
-        4. Quit"""
-        print(menu)
-
     while True:
         start()
 
@@ -32,7 +33,8 @@ def main():
         if user_input == 1:
             print("------")
             for product in product_list:
-                print(f"{product.name}, Price: ${product.price}, Quantity: {product.quantity}")
+                print(f"{product.name}, Price: ${
+                      product.price}, Quantity: {product.quantity}")
             print("------")
             print()
         elif user_input == 2:
@@ -44,24 +46,38 @@ def main():
             total_payment = 0
             print("------")
             for product in product_list:
-                print(f"{product.name}, Price: ${product.price}, Quantity: {product.quantity}")
+                print(f"{product.name}, Price: ${
+                      product.price}, Quantity: {product.quantity}")
             print("------")
             print("When you want to finish order, enter empty text.")
             while True:
                 print()
                 user_input_product = input("Which product do you want? ")
                 user_input_amount = input("What amount do you want? ")
-                if user_input_product in ("1","MacBook Air M2"):
+                if user_input_product in ("1", "MacBook Air M2"):
                     total_payment += product_list[0].price * \
                         int(user_input_amount)
+                    if int(user_input_amount) > product_list[0].quantity:
+                        raise Exception("You have not enough of this item in stock!")
+                    else:
+                        product_list[0].quantity -= int(user_input_amount)
+                        
                     print("Product added to list!")
                 elif user_input_product in ("2", "Bose QuietComfort Earbuds"):
                     total_payment += product_list[1].price * \
                         int(user_input_amount)
+                    if int(user_input_amount) > product_list[1].quantity:
+                        raise Exception("You have not enough of this item in stock!")
+                    else:
+                        product_list[1].quantity -= int(user_input_amount)
                     print("Product added to list!")
                 elif user_input_product in ("3", "Google Pixel 7"):
                     total_payment += product_list[2].price * \
                         int(user_input_amount)
+                    if int(user_input_amount) > product_list[2].quantity:
+                        raise Exception("You have not enough of this item in stock!")
+                    else:
+                        product_list[2].quantity -= int(user_input_amount)
                     print("Product added to list!")
                 elif user_input_product == "" and user_input_amount == "":
                     break
